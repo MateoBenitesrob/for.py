62591a5259e402def5c02a42fd89308ad207ffd9
MateoBenitesrob <mateorob.teo@gmail.com>
2026-06-07 17:52:03 -0500

    for2

--- ejercicio de for.py
+++ ejercicio de for.py
@@ -1,23 +1,41 @@
-productos = {
-    "laptop": 1200,
-    "mouse": 80,
-    "teclado": 150,
-    "monitor": 900,
-    "audifonos": 200
+jugadores = {
+            "mateo": 250,
+             "alex": 180,
+             "sofia": 320,
+             "lukas": 90,
+             "maria": 210
 }
-producto_caro = 0
-valor_premiums = 0
-nombre_del_producto_caro = ""
-contador = 0
-for producto in productos:
-    print(producto, "cuesta", productos[producto], "doláres.")
-    if productos[producto] >= 500:
-        valor_premiums  += productos[producto]
-        contador += 1
-        if productos[producto] > producto_caro:
-            producto_caro =  productos[producto]
-            nombre_del_producto_caro = producto
-    print("------------------------------------------")
-print("hay", contador, "productos premium:")
-print("el valor total de los productos premiums son de: ", valor_premiums, "doláres.")
-print("Nuesto producto estrella es la", nombre_del_producto_caro, "con", producto_caro, "doláres." )
+
+mejor_jugador = ""
+mayor_puntos = 0
+puntos_mínimo = 999999
+peor_jugador = ""
+total = 0
+promedio = 0
+for jugador in jugadores:
+
+
+    if jugadores[jugador] > mayor_puntos:
+        mayor_puntos = jugadores[jugador]
+        mejor_jugador = jugador
+
+    if jugadores[jugador] < puntos_mínimo:
+        puntos_mínimo = jugadores[jugador]
+        peor_jugador = jugador
+    total += jugadores[jugador]
+promedio = total / 5
+
+for jugador in jugadores:
+
+
+    if jugadores[jugador] >= promedio:
+        print(jugador, "tiene más del promedio")
+        print("---------------------------")
+        
+    elif jugadores[jugador] < promedio:
+        print(jugador, "tiene menos del promedio")
+        print("---------------------------")
+    
+print(mejor_jugador, "es la jugadora con más puntos:",mayor_puntos)
+print(peor_jugador,"es el jugador con menos puntos:", puntos_mínimo)
+print("el total de puntos recaudados fueron", total, "puntos")
